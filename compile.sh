90355c528354f267d8f0776fb68491f583510206
Oskar <12718920+oskar2517@users.noreply.github.com>
2025-11-24 00:04:11 +0100

    add compile script

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+gcc -E -P -x c spl_compiler.spl -o spl_compiler_processed.spl
+java -jar shadow-1.0-SNAPSHOT-all.jar spl_compiler_processed.spl --target x86 --headless -o spl_compiler.nasm
+nasm -f elf64 spl_compiler.nasm -o spl_compiler.o
+gcc spl_compiler.o stdlib.o -o spl_compiler -lSDL2 -no-pie
+chmod +x spl_compiler
+ulimit -s 16384
+cat input.spl | ./spl_compiler
