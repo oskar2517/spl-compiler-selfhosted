28a8c1bbe40a2fc7d2ea899b37306d1445ad53a5
Oskar <12718920+oskar2517@users.noreply.github.com>
2025-12-04 04:45:26 +0100

    refactored codebase

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gcc -E -P -x c spl_compiler.spl -o spl_compiler_processed.spl
+gcc -E -P -x c src/spl_compiler.spl -o spl_compiler_processed.spl
 java -jar shadow-1.0-SNAPSHOT-all.jar spl_compiler_processed.spl --target x86 --headless -o spl_compiler.nasm
 nasm -f elf64 spl_compiler.nasm -o spl_compiler.o
 gcc spl_compiler.o stdlib.o -o spl_compiler -lSDL2 -no-pie
